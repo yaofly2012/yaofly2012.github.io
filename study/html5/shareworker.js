097fb8c785490faef7aeea11da5e101aae59eb7c
q_yao <q_yao@Ctrip.com>
2016-06-24 19:36:01 +0800

    mssage worker

--- study/html5/shareworker.js
+++ study/html5/shareworker.js
@@ -1,8 +1,28 @@
+
+var messageHandle = function(e){
+	var workerResult = 0;
+	if(e.data.op === '+'){
+		workerResult = e.data.x + e.data.y;
+	} else if(e.data.op === '*'){
+		workerResult = e.data.x * e.data.y;
+	}
+	
+  	e.source.postMessage({result: workerResult, x:e.data.x, y: e.data.y});
+};
 onconnect = function(e){
-	var port = e.ports[0];
+	var port = e.ports[0]; //e.source
+	//port.onmessage = messageHandle;
 	port.addEventListener('message', function(e){
-		var workerResult = 'Result: ' + (e.data.x * e.data.y);
-      	port.postMessage({result: workerResult});
+		var workerResult = 0;
+		if(e.data.op === '+'){
+			workerResult = e.data.x + e.data.y;
+		} else if(e.data.op === '*'){
+			workerResult = e.data.x * e.data.y;
+		}
+		
+	  	port.postMessage({result: workerResult});
 	});
+	port.start(); // Port要start, 通过onmessage属性赋值内部调用，但是如果显示调用addEventListener('message'),则必须显示调用start
+	
+	port.postMessage({result: 'ready'});
 }
-var a = window();
