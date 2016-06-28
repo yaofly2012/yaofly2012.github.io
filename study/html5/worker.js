11b03eedaeefdf7bd83afb2002a6151e91071626
q_yao <q_yao@Ctrip.com>
2016-06-28 19:05:05 +0800

    worker

--- study/html5/worker.js
+++ study/html5/worker.js
@@ -1,24 +1,8 @@
-// var i=0;
-// var timeHandle = null;
-// var self = this;
-// this.addEventListener('message', function(e){
-// 	i = e.data.count;
-// 	if(e.data.op === 'start'){
-// 		timedCount();
-// 	} else{
-// 		timeHandle && clearTimeout(timeHandle);
-// 		timeHandle = null;
-// 	}
-// });
+var i=0;
 
-// function timedCount()
-// {
-// 	i=i+1;
-// 	postMessage({count: i});
-// 	timeHandle = setTimeout("timedCount()", 1000);
-// }
-postMessage("I\'m working before postMessage(\'ali\').");
-
-onmessage = function (oEvent) {
-  postMessage("Hi " + oEvent.data);
-};
+function timedCount()
+{
+	postMessage(++i);
+	timeHandle = setTimeout(timedCount, 1000);
+}
+timedCount();
