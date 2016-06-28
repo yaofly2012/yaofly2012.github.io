602f46254e18124fd3c9bfe2e01568ff83cd26f2
q_yao <q_yao@Ctrip.com>
2016-06-28 20:05:54 +0800

    worker

--- study/html5/worker.js
+++ study/html5/worker.js
@@ -1,3 +1,16 @@
+// Demo1
+// var i=0;
+// var port2;
+// onmessage = function(e){
+// 	port2 = e.ports[0];
+// 	timedCount();
+// }
+// function timedCount()
+// {
+// 	port2.postMessage(++i);
+// 	timeHandle = setTimeout(timedCount, 1000);
+// }
+
 var i=0;
 function timedCount()
 {
