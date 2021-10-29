b6115677feb4afb82679b82c22b7527cc1617568
q_yao <q_yao@ctrip.com>
2021-10-29 18:36:08 +0800

    add url 结构图示

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # yaofly2012.github.io
-#Node:
-+ 访问静态页面居然大小写敏感！页面“study/css/imgGap.html”居然通过“study/css/imggap.html”访问不到
+# Node:
+1. 访问静态页面居然大小写敏感！
--- study/css/atr.js
+++
@@ -1,6 +0,0 @@
-Shape  = {}
-
-Shape.startPoint // Line(x, y) Rect(x, y) Circle(rx, ry) ellipse(rx, ry)
-Shape.move = function(mX, mY){}
-Shape.draw = function(){} //
-Shape.updateView = function(){}
